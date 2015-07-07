4bb1c17d06e245862bdd1e08ce1804fcfcf5b118
Imanol Gómez <imanolgo@imanolgo.local>
2015-07-07 22:41:14 +0200

    Added led_list with all the LED coordinates

--- src/Main/AppManager.cpp
+++ src/Main/AppManager.cpp
@@ -50,7 +50,7 @@ void AppManager::setup()
 
 void AppManager::setupOF()
 {
-   ofSetVerticalSync(false);
+   ofSetVerticalSync(true);
    ofDisableAlphaBlending();
    ofDisableSmoothing();
    ofDisableDepthTest();

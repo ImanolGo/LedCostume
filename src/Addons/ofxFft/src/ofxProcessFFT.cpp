dd49080eb912556ed119b7940bb61de108cbfa0a
Imanol Gómez <imanolgo@imanolgo-fritz-box.fritz.box>
2015-08-12 10:22:16 +0200

    Added teensy project folder

--- src/Addons/ofxFft/src/ofxProcessFFT.cpp
+++ src/Addons/ofxFft/src/ofxProcessFFT.cpp
@@ -56,7 +56,7 @@ void ProcessFFT::calculateFFT(vector<float>&buffer, float _FFTpercentage, int _n
     this->numFFTbins = _numFFTbins;
     this->FFTpercentage = _FFTpercentage;
 
-    float avgCol[numFFTbins];
+    //float avgCol[numFFTbins];
     
     fftSpectrum.clear(); //empty it all
     
